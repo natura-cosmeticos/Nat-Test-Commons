628ebc85980cfce191404e0f5cd701eff97f806d
Lucas Conceição <lucasdsdc@gmail.com>
2020-07-02 16:04:50 -0300

    Add footer and value TextField assertions
    Co-authored-by: Eduardo Dornel Ribas <eduardo.dornel@hotmail.com>

--- testcommons/src/main/kotlin/net/natura/testcommons/assertions/TextFieldAssertions.kt
+++ testcommons/src/main/kotlin/net/natura/testcommons/assertions/TextFieldAssertions.kt
@@ -23,3 +23,41 @@ fun hasErrorInTextField(expectedText: String) = ViewAssertion { view, noMatching
     assertEquals(TextField.State.ERROR, view.state)
     assertEquals(expectedText, view.error)
 }
+
+/**
+ * Checks if the TextField has a given text.
+ *
+ * Example usage:
+ * ```
+ * onView(withId(R.id.the_textfield_id)).check(hasValueInTextField("my text"))
+ * ```
+ * @param expectedText The text that is expected to be displayed as content.
+ */
+fun hasValueInTextField(expectedText: String) = ViewAssertion { view, noMatchingViewException ->
+    noMatchingViewException?.let { throw it }
+
+    if (view !is TextField) {
+        throw AssertionError("View is not an instance of TextField")
+    }
+
+    assertEquals(expectedText, view.editTextView.text.toString())
+}
+
+/**
+ * Checks if the TextField has a given text as footer.
+ *
+ * Example usage:
+ * ```
+ * onView(withId(R.id.the_textfield_id)).check(hasFooterInTextField("my footer message"))
+ * ```
+ * @param expectedText The text that is expected to be displayed as footer message.
+ */
+fun hasFooterInTextField(expectedText: String) = ViewAssertion { view, noMatchingViewException ->
+    noMatchingViewException?.let { throw it }
+
+    if (view !is TextField) {
+        throw AssertionError("View is not an instance of TextField")
+    }
+
+    assertEquals(expectedText, view.footer)
+}
